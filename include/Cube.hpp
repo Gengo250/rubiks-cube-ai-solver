676b39d16790a94c13573a19b59b7fae1caee5ed
Gengo250 <miguelgengo28@gmail.com>
2026-08-28 20:25:39 -0300

    Fix: add variable_dimension class Cube

--- include/Cube.hpp
+++ include/Cube.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <array>
+
+enum class Color { 
+  White, 
+  Yellow, 
+  Green, 
+  Blue, 
+  Red, 
+  Orange 
+};
+
 class Cube {
-public:
-private:
+ public:
+
+ private:
+  static constexpr int FACE_SIZE = 2;
+  static constexpr int FACE_COUNT = 6;
+  static constexpr int STICKERS_PER_FACE = 4;
+
+  std::array<std::array<Color, STICKERS_PER_FACE>, FACE_COUNT> faces;
 };
