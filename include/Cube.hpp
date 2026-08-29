340d15c127fe1e4b641a900bfb41ca181c65657d
Gengo250 <miguelgengo28@gmail.com>
2026-08-29 09:39:10 -0300

    feat: add cube state and solved initialization

--- include/Cube.hpp
+++ include/Cube.hpp
@@ -10,14 +10,23 @@ enum class Color {
   Red, 
   Orange 
 };
-
+enum class Face{
+  Upper,
+  Lower,
+  Front,
+  Rear, 
+  Right,
+  Left,
+};
 class Cube {
  public:
+
+  Cube();
 
  private:
   static constexpr int FACE_SIZE = 2;
   static constexpr int FACE_COUNT = 6;
-  static constexpr int STICKERS_PER_FACE = 4;
+  static constexpr int STICKERS_PER_FACE = FACE_SIZE * FACE_SIZE;
 
   std::array<std::array<Color, STICKERS_PER_FACE>, FACE_COUNT> faces;
 };
---
+++ src/Cube.cpp
@@ -0,0 +1,24 @@
+#include "Cube.hpp"
+#include <cstddef>
+
+Cube::Cube(){
+  auto upper = static_cast<std::size_t>(Face::Upper);
+  faces[upper].fill(Color::White);
+
+  auto lower = static_cast<std::size_t>(Face::Lower);
+  faces[lower].fill(Color::Yellow);
+
+  auto front = static_cast<std::size_t>(Face::Front);
+  faces[front].fill(Color::Green);
+
+  auto rear = static_cast<std::size_t>(Face::Rear);
+  faces[rear].fill(Color::Blue);
+
+  auto right = static_cast<std::size_t>(Face::Right);
+  faces[right].fill(Color::Red);
+
+  auto left = static_cast<std::size_t>(Face::Left);
+  faces[left].fill(Color::Orange);
+}
+
+
